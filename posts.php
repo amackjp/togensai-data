34e51c5b1eeb8c00275e17034ab58a33570b6cb8
amack <65822003+amackjp@users.noreply.github.com>
2022-04-20 22:36:27 +0900

    Update posts.php

--- posts.php
+++ posts.php
@@ -3,6 +3,6 @@ header('Access-Control-Allow-Origin: *');
 header("Content-Type: application/json; charset=utf-8");
 ?>
 {
- "post2": "CckTDUfJIRF",
- "post1": "CcSO2iIpvu7"
+ "post2": "https://www.instagram.com/p/CckTDUfJIRF/",
+ "post1": "https://www.instagram.com/p/CcSO2iIpvu7/"
 }
