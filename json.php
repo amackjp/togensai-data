6d53241fd63f5f3e0d2e9d6ec7af6cf60775fc73
amack <65822003+amackjp@users.noreply.github.com>
2022-05-06 22:21:45 +0900

    Update and rename posts.php to json.php

--- json.php
+++ json.php
@@ -5,5 +5,7 @@ header("Content-Type: application/json; charset=utf-8");
 {
  "post1": "https://www.instagram.com/p/CdDeGokJdi8/",
  "post2": "https://www.instagram.com/p/Cc7GounpMLP/",
- "post3": "https://www.instagram.com/p/CckTDUfJIRF/"
+ "post3": "https://www.instagram.com/p/CckTDUfJIRF/",
+ "notice": "お知らせはここに掲載されます。",
+ "lost": "心当たりのある方は3階物理教室へ。"
 }
