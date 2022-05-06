6f7f0dcb358b6e14100b466024ec546e7e6ec7f0
amack <65822003+amackjp@users.noreply.github.com>
2022-05-06 15:59:37 +0900

    update

--- posts.php
+++ posts.php
@@ -3,7 +3,7 @@ header('Access-Control-Allow-Origin: *');
 header("Content-Type: application/json; charset=utf-8");
 ?>
 {
- "post1": "https://www.instagram.com/p/CckTDUfJIRF/",
- "post2": "https://www.instagram.com/p/CcSO2iIpvu7/",
- "post3": "https://www.instagram.com/p/CQus4Grs8d6/"
+ "post1": "https://www.instagram.com/p/CdDeGokJdi8/",
+ "post2": "https://www.instagram.com/p/Cc7GounpMLP/",
+ "post3": "https://www.instagram.com/p/CckTDUfJIRF/"
 }
