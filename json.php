e140bcf6462ee5f97bae57d88f985a435f3a3088
amackjp <amackjp@gmail.com>
2022-05-11 22:55:07 +0900

    new post and new topic

--- json.php
+++ json.php
@@ -3,9 +3,9 @@ header('Access-Control-Allow-Origin: *');
 header("Content-Type: application/json; charset=utf-8");
 ?>
 {
- "post1": "https://www.instagram.com/p/CdDeGokJdi8/",
- "post2": "https://www.instagram.com/p/Cc7GounpMLP/",
- "post3": "https://www.instagram.com/p/CckTDUfJIRF/",
- "notice": "お知らせはここに掲載されます。",
+ "post1": "https://www.instagram.com/p/CdNRpytJ7yT/",
+ "post2": "https://www.instagram.com/p/CdDeGokJdi8/",
+ "post3": "https://www.instagram.com/p/Cc7GounpMLP/",
+ "notice": "「お知らせ・お願い」ページに情報委員会からの連絡を掲載しました。",
  "lost": "心当たりのある方は3階物理教室へ。"
 }
