462f9c6d750d6f8ab3199bcc6e8c1a3df28fcecc
amack <65822003+amackjp@users.noreply.github.com>
2022-05-12 17:17:01 +0900

    new posts

--- json.php
+++ json.php
@@ -3,9 +3,9 @@ header('Access-Control-Allow-Origin: *');
 header("Content-Type: application/json; charset=utf-8");
 ?>
 {
- "post1": "https://www.instagram.com/p/CdNRpytJ7yT/",
- "post2": "https://www.instagram.com/p/CdDeGokJdi8/",
- "post3": "https://www.instagram.com/p/Cc7GounpMLP/",
+ "post1": "https://www.instagram.com/p/CdcSwmnpW_B/",
+ "post2": "https://www.instagram.com/p/CdcSsckJmPz/",
+ "post3": "https://www.instagram.com/p/CdNRpytJ7yT/",
  "notice": "「お知らせ・お願い」ページに情報委員会からの連絡を掲載しました。",
  "lost": "心当たりのある方は3階物理教室へ。"
 }
