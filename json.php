aa1571b71480f4d4a3307577a6bdc50adf68dbf0
amack <65822003+amackjp@users.noreply.github.com>
2022-05-14 08:51:25 +0900

    Update json.php

--- json.php
+++ json.php
@@ -6,6 +6,6 @@ header("Content-Type: application/json; charset=utf-8");
  "post1": "https://www.instagram.com/p/CdcSwmnpW_B/",
  "post2": "https://www.instagram.com/p/CdcSsckJmPz/",
  "post3": "https://www.instagram.com/p/CdNRpytJ7yT/",
- "notice": "【NEW】2日目好評につき予約枠を午前・午後それぞれ200名追加します！追加申込は5月15日(日)9時より開始します！\\n生徒向け:「お知らせ・お願い」ページに情報委員会からの連絡を掲載しました。",
+ "notice": "【NEW】2日目好評につき予約枠を午前・午後それぞれ200名追加します！追加申込は5月15日(日)9時より開始します！",
  "lost": "心当たりのある方は3階物理教室へ。"
 }
