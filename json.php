3e96d06b1fe7b37e4036591ff44e8a3955f95334
amack <65822003+amackjp@users.noreply.github.com>
2022-05-15 12:42:11 +0900

    Update json.php

--- json.php
+++ json.php
@@ -3,9 +3,9 @@ header('Access-Control-Allow-Origin: *');
 header("Content-Type: application/json; charset=utf-8");
 ?>
 {
- "post1": "https://www.instagram.com/p/CdcSwmnpW_B/",
- "post2": "https://www.instagram.com/p/CdcSsckJmPz/",
- "post3": "https://www.instagram.com/p/CdNRpytJ7yT/",
+ "post1": "https://www.instagram.com/p/Cdfk8GqJkbV/"
+ "post2": "https://www.instagram.com/p/CdcSwmnpW_B/",
+ "post3": "https://www.instagram.com/p/CdcSsckJmPz/",,
  "notice": "【NEW】2日目好評につき予約枠を午前・午後それぞれ200名追加します！追加申込は5月15日(日)9時より開始します！",
  "lost": "心当たりのある方は3階物理教室へ。"
 }
