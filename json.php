f7b4256f024b4690024b99ecc53d033f1001d1a9
amack <65822003+amackjp@users.noreply.github.com>
2022-05-19 10:23:16 +0900

    Update json.php

--- json.php
+++ json.php
@@ -3,9 +3,9 @@ header('Access-Control-Allow-Origin: *');
 header("Content-Type: application/json; charset=utf-8");
 ?>
 {
- "post1": "https://www.instagram.com/p/CdpgWDaJ0bB/",
- "post2": "https://www.instagram.com/p/CdnLy96pwPP/",
- "post3": "https://www.instagram.com/p/Cdfk8GqJkbV/",
- "notice": "[お知らせ・お願い]ページに保護者の方向けのフードコートのお知らせを掲載しました。",
+ "post1": "https://www.instagram.com/p/CdsEhNwpQZJ/",
+ "post2": "https://www.instagram.com/p/CdpgWDaJ0bB/",
+ "post3": "https://www.instagram.com/p/CdnLy96pwPP/",
+ "notice": "[お知らせ・お願い]ページに保護者の方向けのフードコートとPTAの制服リユースのお知らせを掲載しました。",
  "lost": "心当たりのある方は本部(3階物理教室)へ。"
 }
